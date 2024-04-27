5952630cc70eafcab6079fee1238295459e97697
Chris Knight <chrisk314@gmail.com>
2024-04-27 11:25:50 +0100

    Adds nav bar event handler functions to page script

--- csknight.dev/src/page-script.js
+++ csknight.dev/src/page-script.js
@@ -18,3 +18,36 @@ window.onload = function () {
     }
   }, 1000)
 }
+
+var nav = document.getElementById('nav');
+var mytopnav = document.getElementById('myTopnav');
+var myicon = document.getElementById('myIcon');
+var mydropdown = document.getElementById('myDropdown');
+
+mytopnav.menuIsOpen = false;
+mytopnav.subMenuIsOpen = false;
+
+function toggleDropdownMainMenu() {
+    mytopnav.menuIsOpen = !mytopnav.menuIsOpen;
+    if (mytopnav.menuIsOpen) {
+        if (mytopnav.subMenuIsOpen) {
+            toggleDropdownSubMenu();
+        }
+        mytopnav.classList.add("menu-open");
+        myicon.classList.add("menu-open");
+    } else {
+        mytopnav.classList.remove("menu-open");
+        myicon.classList.remove("menu-open");
+    }
+};
+
+function toggleDropdownSubMenu() {
+    if (mytopnav.menuIsOpen) {
+        mytopnav.subMenuIsOpen = !mytopnav.subMenuIsOpen;
+        if (mytopnav.subMenuIsOpen) {
+            mydropdown.classList.add("submenu-open");
+        } else {
+            mydropdown.classList.remove("submenu-open");
+        }
+    };
+};
